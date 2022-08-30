19cb5eecd6f6cc0013ffa942ea2b6f0aee6c0bd5
Taiel Figueroa <taiel_tls@hotmail.com>
2022-08-30 10:58:41 -0300

    Primera subida

---
+++ ejemplo1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
